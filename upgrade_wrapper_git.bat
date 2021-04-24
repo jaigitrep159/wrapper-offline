f737358686d8897e341d5b85e2f927504bc2aafb
Keegan J.S <62641690+imageny@users.noreply.github.com>
2021-04-23 23:45:41 -0500

    1.3.0 (I FORGOT THE UPGRADER OH GOD)

---
+++ upgrade_wrapper_git.bat
@@ -0,0 +1,31 @@
+@echo off
+:: Wrapper Offline Updater
+:: Author: xomdjl_#1337 (ytpmaker1000@gmail.com)
+
+echo Welcome to the Wrapper: Offline updater.
+echo:
+echo This will update your copy of Wrapper: Offline to
+echo the latest build using Git.
+echo:
+echo To continue, press 1.
+echo Otherwise, press any key.
+echo:
+set /p GITPULL= Option:
+if "%GITPULL%"=="1" (
+	cls
+	echo Pulling latest version of repository from GitHub through Git...
+	PING -n 4 127.0.0.1>nul
+	call git pull
+	echo:
+	echo Latest version of repository pulled^!
+	echo:
+	pause
+)
+if "%GITPULL%"=="" (
+	echo NO UPDATE FOR YOU^!
+	PING -n 5 127.0.0.1>nul
+	echo COME BACK, ONE YEAR^!
+	PING -n 4 127.0.0.1>nul
+	echo:
+	pause & exit
+)

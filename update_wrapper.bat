953c81b10b0e02b5e8201253b3516d2f67b1d405
Keegan S <ytpmaker1000@gmail.com>
2021-05-13 10:09:52 -0500

    configured the updater some more

--- update_wrapper.bat
+++ update_wrapper.bat
@@ -1,6 +1,9 @@
-@echo off
 :: Wrapper Offline Updater
 :: Author: xomdjl_#1337 (ytpmaker1000@gmail.com)
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+set SUBSCRIPT=y
+call utilities\config.bat
 
 echo Welcome to the Wrapper: Offline updater.
 popd
@@ -24,46 +27,93 @@ if "%GITPULL%"=="1" (
 				for /f "delims=" %%i in ('git^>nul') do set output=%%i
 				echo:
 				IF "%output%" EQU "" (
+					if "%DEVMODE%"=="y" (
+						echo Git couldn't be found anywhere, but you do have developer mode on.
+						echo:
+						echo If this is an actual dev using this from another machine who
+						echo hasn't installed the actual Git yet, would you like to go ahead
+						echo and install it?
+						echo:
+						echo Press 1 if you'd like to install actual Git
+						echo Otherwise, press Enter
+						set /p GITRES= Response:
+						if "%GITRES%"=="1" (
+							echo Launching the Git installer...
+							start utilities\git.exe
+							echo Git installer launched^!
+							echo:
+							echo Once installation is finished, press any key to go to the
+							echo next step of updating 
+							:gitrecheck
+							pause
+							for /f "delims=" %%i in ('git^>nul') do set output1=%%i
+							IF "%output1%" EQU "" (
+								echo Git not found. Please keep the installation going.
+								echo When finished, press any key to continue.
+								echo:
+								goto gitrecheck
+							) else (
+								TASKKILL /F /IM git.exe>nul
+								echo Git has been installed^! Proceeding to update...
+								PING -n 4 127.0.0.1>nul
+								cls
+								goto update
+							)
+					)
+					:nosignofgit
 					echo Okay, there's no sign of Git ANYWHERE on this computer.
 					PING -n 4 127.0.0.1>nul
 					echo That means YOU MUST HAVE INSTALLED THIS INCORRECTLY!
 					PING -n 4 127.0.0.1>nul
-					set GITPULL=""
+					goto noupdate
 				) else (
+					set GIT=y
 					echo The command-line worked, therefore Git is installed.
 					echo:
+					goto update
 				)
 			)
 		) else (
+			set GIT=y
 			echo Git was detected in one of the Program Files folders, therefore it is installed.
 			echo:
+			goto update
 		)
-		echo Saving custom settings in temporary file...
-		pushd utilities
-		copy config.bat tmpcfg.bat>nul
-		popd
-		echo:
-		echo Pulling latest version of repository from GitHub through Git...
-		PING -n 4 127.0.0.1>nul
-		echo:
-		call git pull
-		echo:
-		echo Deleting config.bat from repository and replacing it with user's copy...
-		pushd utilities
-		del config.bat
-		ren tmpcfg.bat config.bat
-		popd
-		echo:
-		echo Latest version of repository pulled^!
-		echo:
-		pause
+		:update
+		if "%GIT%"=="y" (
+			echo Saving custom settings in temporary file...
+			pushd utilities
+			copy config.bat tmpcfg.bat>nul
+			popd
+			echo:
+			echo Pulling latest version of repository from GitHub through Git...
+			PING -n 4 127.0.0.1>nul
+			echo:
+			call git pull
+			echo:
+			echo Deleting config.bat from repository and replacing it with user's copy...
+			pushd utilities
+			del config.bat
+			ren tmpcfg.bat config.bat
+			popd
+			echo:
+			echo Latest version of repository pulled^!
+			echo:
+			pause
+		)
 	)
+) else (
+	set GIT=n
+	goto noupdate
 )
-if "%GITPULL%"=="" (
+
+
+:noupdate
+if "%GIT%"=="n" (
 	echo NO UPDATE FOR YOU^!
 	PING -n 5 127.0.0.1>nul
 	echo COME BACK, ONE YEAR^!
 	PING -n 4 127.0.0.1>nul
 	echo:
-	pause
+	pause & exit
 )

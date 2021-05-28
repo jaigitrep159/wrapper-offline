7a8ab06deabe3494cda483a115f13477c5788c23
narutofan420 <73909524+narutofan420@users.noreply.github.com>
2021-05-27 23:45:00 -0500

    Just because the user has Git installed, does not mean that they cloned the repo.

--- update_wrapper.bat
+++ update_wrapper.bat
@@ -1,139 +1,125 @@
-:: Wrapper Offline Updater
+:: Automatically upgrades an existing W:O install to a new version
 :: Author: xomdjl_#1337 (ytpmaker1000@gmail.com)
-@echo off
+:: Original idea from 2Epik4u#1904
+:: License: MIT
+
+:: Initialize (stop command spam, clean screen, make variables work, set to UTF-8)
+@echo off && cls
 SETLOCAL ENABLEDELAYEDEXPANSION
+chcp 65001 >nul
+
+:: Move to base folder, and make sure it worked (otherwise things would go horribly wrong)
+pushd "%~dp0"
+if !errorlevel! NEQ 0 goto error_location
+if not exist utilities ( goto error_location )
+if not exist wrapper ( goto error_location )
+if not exist server ( goto error_location )
+goto noerror_location
+:error_location
+echo Doesn't seem like this script is in a Wrapper: Offline folder.
+goto end
+:noerror_location
+
+:: Prevents CTRL+C cancelling and keeps window open when crashing
+if "%~1" equ "point_insertion" goto point_insertion
+start "" /wait /B "%~F0" point_insertion
+exit
+:point_insertion
+
+:: patch detection
+if exist "patch.jpg" echo no amount of upgrades can fix a patch && goto end
+
+:: Get config.bat
 set SUBSCRIPT=y
+if not exist utilities\config.bat ( goto error_location )
 call utilities\config.bat
-echo Welcome to the Wrapper: Offline updater.
+
+
+echo Would you like to upgrade?
 echo:
-echo This will update your copy of Wrapper: Offline to
-echo the latest build using Git.
+echo Press Y to install the update, press N to cancel.
 echo:
-echo To continue, press 1.
-echo Otherwise, press any key.
+:installaskretry
+set /p INSTALLCHOICE= Response:
 echo:
-set /p GITPULL= Option:
-if "%GITPULL%"=="1" (
-		cls
-		echo Checking for Git installation...
-		echo:
-		if not exist "%PROGRAMFILES%\Git" (
-			if not exist "%PROGRAMFILES(X86)%\Git" (
-				echo Could not detect Git in Program Files folders.
-				echo Checking via command-line testing...
-				for /f "delims=" %%i in ('git --version 2^>nul') do set output=%%i
-				echo:
-				PING -n 4 127.0.0.1>nul
-				IF "%output%" EQU "" (
-					if "%DEVMODE%"=="y" (
-						echo Git couldn't be found anywhere, but you do have developer mode on.
-						echo:
-						echo If either this is an actual dev using this from another machine who
-						echo hasn't installed the actual Git yet, or this is someone who cloned
-						echo the repository using something like GitHub Desktop, would you like
-						echo to go ahead and install it?
-						echo:
-						echo Press 1 if you'd like to install actual Git
-						echo Otherwise, press 2.
-						echo:
-						:gitresretry
-						set /p GITRES= Response:
-						if "%GITRES%"=="1" (
-							cls
-							echo Launching the Git installer...
-							start utilities\git.exe
-							PING -n 4 127.0.0.1>nul
-							echo Git installer launched^^!
-							echo:
-							echo Once installation is finished, press any key to go to the
-							echo next step of updating.
-							echo:
-							:gitrecheck
-							pause
-							for /f "delims=" %%i in ('git --version 2^>nul') do set output1=%%i
-							IF "%output1%" EQU "" (
-								echo Git not found. Please keep the installation going.
-								echo When finished, press any key to continue.
-								echo:
-								goto gitrecheck
-							) else (
-								TASKKILL /F /IM git.exe>nul
-								echo Git has been installed^! Proceeding to update...
-								PING -n 4 127.0.0.1>nul
-								cls
-								set GIT=y
-								goto update
-							)
-						)
-						if "%GITRES%"=="2" ( goto nosignofgit )
-						if "%GITRES%"=="" ( echo You must choose a valid response. && goto gitresretry )
-					) else goto nosignofgit
-				) else (
-					set GIT=y
-					echo The command-line worked, therefore Git is installed.
-					echo:
-					goto update
-				)
-			)
-		) else (
-			set GIT=y
-			echo Git was detected in one of the Program Files folders, therefore it is installed.
-			echo:
-			goto update
-		)
-	)
-	:update
-	if "%GIT%"=="y" (
-		echo Saving custom settings in temporary file...
-		pushd utilities
-		copy config.bat tmpcfg.bat>nul
-		popd
-		echo Saving imported assets to temporary files...
-		call utilities\7za.exe a "utilities\misc\temp\importarchive.zip" .\server\store\3a981f5cb2739137\import\*>nul
-		echo Pulling latest version of repository from GitHub through Git...
-		PING -n 4 127.0.0.1>nul
-		call git pull
-		echo Deleting config.bat from repository and replacing it with user's copy...
-		pushd utilities
-		del config.bat
-		ren tmpcfg.bat config.bat
-		popd
-		echo Deleting all the imported assets from the repository and replacing it with user's assets...
-		pushd server\store\3a981f5cb2739137
-		rd /q /s import
-		md import
-		popd
-		call utilities\7za.exe e "utilities\misc\temp\importarchive.zip" -o"server\store\3a981f5cb2739137\import" -y>nul
-		del utilities\misc\temp\importarchive.zip>nul
-		del "wrapper\_THEMES\import.xml">nul
-		copy "server\store\3a981f5cb2739137\import\theme.xml" "wrapper\_THEMES\import.xml">nul
-		echo Latest version of repository pulled^^!
-		echo:
-		pause
-		goto end
-	)
+if not '!installchoice!'=='' set installchoice=%installchoice:~0,1%
+if /i "!installchoice!"=="0" goto end
+if /i "!installchoice!"=="y" goto checkforgit
+if /i "!installchoice!"=="n" goto end
+echo You must answer Yes or No. && goto installaskretry
+
+:checkforgit
+if !VERBOSEWRAPPER!==n ( cls )
+echo Checking if you downloaded Wrapper: Offline correctly...
+if exist .git (
+	if !VERBOSEWRAPPER!==n ( cls )
+	echo Git folder has been found!
+	echo Beginning update...
+	echo:
+	goto startupdate
 ) else (
-	set GIT=n
-	goto noupdate
+	goto nogit
 )
 
-:nosignofgit
-echo Okay, there's no sign of Git ANYWHERE on this computer.
+:nogit
+if !VERBOSEWRAPPER!==n ( cls )
+echo Okay, there's no sign of Wrapper: Offline being
+echo cloned through the installer.
 PING -n 4 127.0.0.1>nul
 echo That means YOU MUST HAVE INSTALLED THIS INCORRECTLY^^!
 PING -n 4 127.0.0.1>nul
-set GIT=n
-goto noupdate
+goto endseinfeld
 
-:noupdate
-if "%GIT%"=="n" (
-	echo NO UPDATE FOR YOU^^!
-	PING -n 5 127.0.0.1>nul
-	echo COME BACK, ONE YEAR^^!
-	PING -n 4 127.0.0.1>nul
-	echo:
-	pause
-)
-
-:end
+:startupdate
+if !VERBOSEWRAPPER!==n ( cls )
+echo Please do not close this window^^!^^!
+echo Doing so may ruin your copy of Wrapper: Offline.
+echo It's almost certainly NOT frozen, just takes a while.
 echo:
+:: Save user data
+if !VERBOSEWRAPPER!==y ( echo Saving custom settings in temporary file... )
+pushd utilities
+copy config.bat tmpcfg.bat>nul
+popd
+if !VERBOSEWRAPPER!==y ( echo Saving imported assets to temporary files... )
+call utilities\7za.exe a "utilities\misc\temp\importarchive.zip" .\server\store\3a981f5cb2739137\import\*>nul
+if !VERBOSEWRAPPER!==y ( echo Pulling latest version of repository from GitHub through Git... )
+PING -n 4 127.0.0.1>nul
+:: Perform the update
+call git pull
+:: Bring back user data
+if !VERBOSEWRAPPER!==y ( echo Deleting config.bat from repository and replacing it with user's copy... )
+pushd utilities
+del config.bat
+ren tmpcfg.bat config.bat
+popd
+if !VERBOSEWRAPPER!==y ( echo Deleting all the imported assets from the repository and replacing it with user's assets... )
+pushd server\store\3a981f5cb2739137
+rd /q /s import
+md import
+popd
+call utilities\7za.exe e "utilities\misc\temp\importarchive.zip" -o"server\store\3a981f5cb2739137\import" -y>nul
+del utilities\misc\temp\importarchive.zip>nul
+del "wrapper\_THEMES\import.xml">nul
+copy "server\store\3a981f5cb2739137\import\theme.xml" "wrapper\_THEMES\import.xml">nul
+
+:: congratulations new version
+if !VERBOSEWRAPPER!==n ( cls )
+color 20
+echo:
+echo:
+echo Update installed^^!
+echo:
+goto end
+
+:: seinfeld reference
+:endseinfeld
+echo NO UPDATE FOR YOU^^!
+PING -n 5 127.0.0.1>nul
+echo COME BACK, ONE YEAR^^!
+PING -n 4 127.0.0.1>nul
+
+:: normal end
+:end
+echo Closing...
+pause & exit

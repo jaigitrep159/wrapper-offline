e8213ef477991598c4ae9ea3d70306ed2541198f
narutofan420 <73909524+narutofan420@users.noreply.github.com>
2021-06-03 18:26:02 -0500

    Update config.bat

--- utilities/config.bat
+++ utilities/config.bat
@@ -8,10 +8,10 @@ if "%SUBSCRIPT%"=="" ( start notepad.exe "%CD%\%~nx0" & exit )
 endlocal
 
 :: Shows exactly Offline is doing, and never clears the screen. Useful for development and troubleshooting. Default: n
-set VERBOSEWRAPPER=y
+set VERBOSEWRAPPER=n
 
 :: Won't check for dependencies (flash, node, etc) and goes straight to launching. Useful for speedy launching post-install. Default: n
-set SKIPCHECKDEPENDS=y
+set SKIPCHECKDEPENDS=n
 
 :: Won't install dependencies, regardless of check results. Overridden by SKIPCHECKDEPENDS. Mostly useless, why did I add this again? Default: n
 set SKIPDEPENDINSTALL=n
@@ -20,7 +20,7 @@ set SKIPDEPENDINSTALL=n
 set INCLUDEDCHROMIUM=y
 
 :: Opens INCLUDEDCHROMIUM in headless mode. Looks pretty nice. Overrides CUSTOMBROWSER and BROWSER_TYPE. Default: y
-set APPCHROMIUM=n
+set APPCHROMIUM=y
 
 :: Opens Offline in a browser of the user's choice. Needs to be a path to a browser executable in quotes. Default: n
 set CUSTOMBROWSER=n
@@ -39,11 +39,11 @@ set CEPSTRAL=n
 set INCLUDEDBASILISK=n
 
 :: Makes it so both the settings and the Wrapper launcher shows developer options. Default: n
-set DEVMODE=y
+set DEVMODE=n
 
 :: Tells settings.bat which port the frontend is hosted on. (If changed manually, you MUST also change the value of "SERVER_PORT" to the same value in wrapper\env.json) Default: 4343
 set PORT=4343
 
-:: Automatically restarts the NPM whenever it crashes. Default: n
+:: Automatically restarts the NPM whenever it crashes. Default: y
 set AUTONODE=y
 

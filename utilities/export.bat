b38bf3642900cc2417ea7133b381e4091354fcd9
Keegan J.S <62641690+imageny@users.noreply.github.com>
2021-06-07 13:30:00 -0500

    Update export.bat

--- utilities/export.bat
+++ utilities/export.bat
@@ -594,41 +594,39 @@ echo:
 :formatretry
 set /p FORMATTYPE= Option: 
 if "%FORMATTYPE%"=="1" (
-set FILESUFFIX=mp4
-set VCODEC=h264
-set ACODEC=aac
-set ADDITIONAL=""
-goto outputcheck
+	set FILESUFFIX=mp4
+	set VCODEC=h264
+	set ACODEC=aac
+	set ADDITIONAL=" -crf %CRF%"
+	goto outputcheck
 )
 if "%FORMATTYPE%"=="2" (
-set FILESUFFIX=avi
-set VCODEC=libx264
-set ACODEC=libmp3lame
-set ADDITIONAL=""
-goto outputcheck
+	set FILESUFFIX=avi
+	set VCODEC=libx264
+	set ACODEC=libmp3lame
+	set ADDITIONAL=""
+	goto outputcheck
 )
 if "%FORMATTYPE%"=="3" (
-set FILESUFFIX=webm
-set VCODEC=libvpx
-set ACODEC=libvorbis
-set ADDITIONAL=""
-goto outputcheck
+	set FILESUFFIX=webm
+	set VCODEC=libvpx
+	set ACODEC=libvorbis
+	set ADDITIONAL=""
+	goto outputcheck
 )
 if "%FORMATTYPE%"=="4" (
-set FILESUFFIX=wmv
-set VCODEC=wmv2
-set ACODEC=wmav2
-set ADDITIONAL=""
-goto outputcheck
+	set FILESUFFIX=wmv
+	set VCODEC=wmv2
+	set ACODEC=wmav2
+	set ADDITIONAL=""
+	goto outputcheck
 )
 echo Invalid option. Please try again. && goto formatretry
 
 :outputcheck
 if "%DEVMODE%"=="y" (
 	if "%VCODEC%"=="h264" ( goto crfvalue )
-) else (
-	goto output
-)
+) else goto output
 
 :crfvalue
 echo ^(Developer mode-exclusive option^)

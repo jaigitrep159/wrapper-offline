7ea76b34a314d3608716230549f5555805b9b878
Keegan J.S <62641690+imageny@users.noreply.github.com>
2021-06-11 22:18:43 -0500

    bug fix

--- start_wrapper.bat
+++ start_wrapper.bat
@@ -35,9 +35,9 @@ if %NOMETA%==n ( set SUBSCRIPT=y & call utilities\metadata.bat )
 
 :rebootasadmin
 if %ADMIN%==n (
-	echo Set UAC = CreateObject^("Shell.Application"^) > %tmp%\requestAdmin.vbs
+	echo Set UAC = CreateObject^("Shell.Application"^)>> %tmp%\requestAdmin.vbs
 	set params= %*
-	echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> %tmp%\requestAdmin.vbs
+	echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1>> %tmp%\requestAdmin.vbs
 	start "" %tmp%\requestAdmin.vbs
 	exit /B
 )
